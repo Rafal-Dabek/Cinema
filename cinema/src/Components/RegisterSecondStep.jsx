ebfd9005c758928ab52edac9c0166690f976e5ba
Rafal-Dabek <62549937+Rafal-Dabek@users.noreply.github.com>
2022-07-13 01:37:31 +0200

    Update RegisterSecondStep.jsx

--- cinema/src/Components/RegisterSecondStep.jsx
+++ cinema/src/Components/RegisterSecondStep.jsx
@@ -61,19 +61,19 @@ function getAge(dateString) {//checks the age of a person
 function isValid(e) {  //checks if a person is 18 or older
    
     
-    if(e.target.name==="date_of_birth"){
+    if(e.target.id==="date_of_birth"){
         (getAge(e.target.value)>=18)? document.getElementById("over18yo").style.color = "green" : document.getElementById("over18yo").style.color = "red";
         (getAge(e.target.value)>=18)? setAgeValid(true) : setAgeValid(false) //checks if age is correct
          
        
     }
-    if(e.target.name==="first_name"){//checks if firstname is correct
+    if(e.target.id==="first_name"){//checks if firstname is correct
         
             (/^[A-Z][a-z0-9_-]{1,19}$/.test(e.target.value))? setfnameValid(true) : setfnameValid(false) 
             
        }
 
-    if(e.target.name==="last_name"){//checks if lastname is correct
+    if(e.target.id==="last_name"){//checks if lastname is correct
         
             (/^[A-Z][a-z0-9_-]{1,19}$/.test(e.target.value))? setlnameValid((prevState,props)=>(  //changes the lastnameValid property
             {
@@ -93,7 +93,7 @@ function changeCheckerValid(){
 
 
 const onChange=(e)=>{
-    setValues({...values,[e.target.name]:e.target.value})
+    setValues({...values,[e.target.id]:e.target.value})
     isValid(e)
 }
 
@@ -107,14 +107,30 @@ const onChange=(e)=>{
                 
                 
                 <form>
-                    {inputs.map((input)=>(
+                    {/*inputs.map((input)=>(
                         <FormInput key={input.id}
                          {...input} 
                         value={values[input.name]} 
                          onChange={onChange}
                          />
 
-                    ))}
+                    ))*/}
+
+<div className='formInput'>
+    <label>First name</label>
+    <input  onChange={onChange} type="text" required="true"  id="first_name" placeholder="e.g. Jessica" />
+</div>
+<div className='formInput' >
+    <label>Last Name</label>
+    <input  onChange={onChange} type= "text"  id="last_name"  required="true" placeholder="e.g. Walton" />
+</div>
+<div className='formInput' >
+    <label>Date of birth</label>
+    <input  onChange={onChange} type= "date"  id="date_of_birth"  required="true"  />
+</div>
+
+
+
                     <p id="over18yo" className='helper'>You should be minimum 18 years old</p>
                     <span id="privacyPolicy"><input type="checkbox" id="privacyPolicyCheckbox" required onClick={changeCheckerValid} />
                     <label for="checkbox" id="privacyPolicyLabel"  > I accept <a id="privacyPolicyLink" href='#'>Privacy Policy</a></label></span>
